4e00095127f74cb3ae10da25f5eeb52aef799807
felipesv <1165@holbertonschool.com>
2020-04-20 16:09:09 -0500

    add task 0 - 5

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# script that start flask
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """function that print Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """function that print HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_something(text):
+    """function that print c <something>"""
+    return "c {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# script that start flask
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """function that print Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """function that print HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_something(text):
+    """function that print c <something>"""
+    return "c {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_python(text="is cool"):
+    """function that print python <something>"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def python_something(n):
+    """function that print <integer> is a number"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_is_number(n):
+    """function that print <integer> is a number"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def template_number(n):
+    """function that print <integer> is a number"""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
