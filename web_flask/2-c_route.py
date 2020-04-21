8096180732bbf94d78183ebb99dfe270ce8f94d5
felipesv <1165@holbertonschool.com>
2020-04-21 17:17:22 -0500

    fix task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_something(text):
     """function that print c <something>"""
-    return "c {}".format(text.replace("_", " "))
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
